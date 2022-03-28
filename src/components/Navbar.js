ee428c8b7c48c35f63ae8cb4a8c976075aaf0912
Jeanet Lin <linett@gmail.com>
2022-03-28 16:50:08 +0200

    iteration 1.2 - fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 export function Navbar() {
     return (
-        <nav className="navbar navbar-dark bg-primary mb-3">
-        <div className="container">
-          <Link className="navbar-brand" to="/">WikiCountries</Link> 
+        <div className="navbar">
+          <Link className="navbar-brand" to="/">
+          <h1>LAB - WikiCountries</h1>
+          </Link> 
           </div>
-      </nav>
     );
 };
