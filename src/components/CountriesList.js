1c87f8045fd911847933760ffa4c0d90ee7522ad
Jeanet Lin <linett@gmail.com>
2022-03-29 15:28:38 +0200

    iteration 1.3 - done (missing center alignment)

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -9,20 +9,23 @@ export function CountriesList() {
     setListCountries(countriesData)
   }, [])
 
-  let imgSrc = `https://flagpedia.net/data/flags/icon/72x54/${countriesData.alpha2Code.toLowerCase()}.png`
-
   return (
-    <div className="col-5">
+      
+    
+    <div className="col-5" style={{maxHeight: "90vh", overflow: "scroll"}}>
       <div className="list-group">
         {listCountries.map((country) => {
           return (
-            <Link to={`${country.alpha3Code}`}>
-              <img src={imgSrc} alt="" />
-              {country.name.official}
+              <div>
+            <Link className="list-group-item list-group-item-action" key={country.alpha3Code} to={`${country.alpha3Code}`} >
+              <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt={`${country.name.official}`} />
+              <p>{country.name.official}</p>
             </Link>
+            </div>
           )
         })}
       </div>
     </div>
+    
   )
 }
